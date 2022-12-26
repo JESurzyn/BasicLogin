83331534580f57208e24835c24157ca8f3e24410
JESurzyn <jordan.surzyn@gmail.com>
2022-12-26 14:17:51 -0800

    basic functionality finished

--- server/app.js
+++ server/app.js
@@ -24,17 +24,58 @@ app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '../client/views'))
 
-app.get('/login', async (req, res) => {
+app.use(express.urlencoded({extended:true}))
+
+app.get('/', async (req, res) => {
     res.render('pages/login')
 })
 
-app.get('/usercreate', async (req, res) => {
-    res.render('pages/usercreate')
-})
-app.get('/home', async (req, res) => {
-    res.render('pages/home')
+app.get('/user/:username', async (req, res) => {
+    const {username} = req.params
+    const userFound = await User.find({username: username})
+    const user = userFound[0]
+    console.log(user)
+    res.render('pages/home', {user})
 })
 
+app.post('/home', async (req, res) => {
+    console.log(req.body)
+    const user = await User.find({username: req.body.username})
+    console.log(user)
+    console.log(user.length)
+    //logic that compares un and un and pw and pw
+    if(user.length< 1){
+        //probably should handle this a little better
+        res.redirect('/')
+    } else {
+        //do credentials comparions
+        if(req.body.username === user[0].username && req.body.password ===user[0].password) {
+            res.redirect(`user/${user[0].username}`);
+        } else {
+            res.redirect('/')
+        }  
+    }
+})
+
+app.get('/signup', async (req, res) => {
+    res.render('pages/usercreate')
+})
+
+app.post('/new', async (req, res) => {
+    const user = new User(req.body.user);
+    const userQuery = await User.find({username: user.username})
+    if (userQuery.length > 0) {
+        //maybe add exception?
+        console.log('ERROR: user exists')
+        res.redirect('/signup')
+    } else {
+        console.log('user doesnt exist')
+        console.log('creating user')
+        await user.save();
+        console.log('redirecting to home')
+        res.redirect(`user/${user.username}`)
+    }
+})
 
 const port = 3000
 app.listen(port, () => {
