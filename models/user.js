cc80553a107709ea7a25e9e2fe78adabcb5e9c76
JESurzyn <jordan.surzyn@gmail.com>
2023-02-15 08:19:01 -0500

    cleaning up

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 //define schema
 const userSchema = new mongoose.Schema({
@@ -22,6 +23,25 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true
     }
+})
+
+userSchema.statics.findAndValidate = async function(username, password) {
+    const foundUser = await this.findOne({username});
+    if(foundUser){
+        const isValid = await bcrypt.compare(password, foundUser.password);
+        return isValid ? foundUser : false;
+    } else {
+        return false;
+    }
+    
+    
+}
+
+//middleware
+userSchema.pre('save', async function (next) {
+    if(!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12)
+    next();
 })
 
 //define model
